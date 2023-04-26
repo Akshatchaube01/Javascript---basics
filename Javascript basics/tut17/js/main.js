f304c63c2cc6bf5b840115bb750b476dc9346016
Akshatchaube01 <92796050+Akshatchaube01@users.noreply.github.com>
2023-04-26 23:27:13 +0530

    updated main1.js

--- Javascript basics/tut17/js/main.js
+++ Javascript basics/tut17/js/main.js
@@ -1,1 +1,22 @@
 //Classes
+class Pizza{
+    constructor(pizzaType,pizzaSize){
+        this.type = pizzaType;
+        this.size = pizzaSize; // Or you can just simply put "medium"
+        this.crust = "original";
+    }
+    get pizzaCrust(){
+        return this.crust;
+    }
+    set pizzaCrust(pizzaCrust){
+        this.crust = pizzaCrust; 
+    }
+    bake(){
+        console.log(`Baking a ${this.size} ${this.type} ${this.crust} baked pizza.`)
+    }
+}
+
+const myPizza=new Pizza("Pepperoni","small");
+myPizza.pizzaCrust = "thin";
+myPizza.bake();
+console.log(myPizza.pizzaCrust);
